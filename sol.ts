f0d757e7b08e092fb4c2f7a20d3256c2b9d52636
Wojcik <dwojcik@luxoft.com>
2026-03-22 10:39:03 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,12 +1,23 @@
-const newFilterModel = Object.fromEntries(
-  filteringSpec?.flatMap(matcher => {
-    const gridFilter = gridFilters?.[matcher.field];
-    if (!gridFilter) {
-      logger.warn(`No grid filter found for colId: ${matcher.field}`);
-      return [];
+const newFilterModel: Record<string, any> = {};
+
+filteringSpec?.forEach(matcher => {
+  const gridFilter = gridFilters?.[matcher.field];
+  if (!gridFilter) {
+    logger.warn(`No grid filter found for colId: ${matcher.field}`);
+    return;
+  }
+  const result = gridFilter.fromFilteringSpecToGridFilterModel(matcher);
+  if (!result) return;
+
+  for (const [key, val] of Object.entries(result)) {
+    if (newFilterModel[key]) {
+      // merge values arrays
+      newFilterModel[key] = {
+        ...newFilterModel[key],
+        values: [...newFilterModel[key].values, ...val.values],
+      };
+    } else {
+      newFilterModel[key] = val;
     }
-    // TODO: rename /w 'matcher'
-    const result = gridFilter?.fromFilteringSpecToGridFilterModel(matcher);
-    return result ? Object.entries(result) : [];
-  }) ?? []
-);
+  }
+});
