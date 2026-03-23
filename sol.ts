ce9adf059bc449b87e07c97e5c4f53a0ecd0fa51
Wojcik <dwojcik@luxoft.com>
2026-03-23 10:15:35 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,6 +1,18 @@
-defaultSelection: [DateTimeFilter.OneMonth],
+for (const [field, extendedColDef] of Object.entries(extendedColDefs)) {
+  const gridFilter = gridFilters?.[field];
+  const defaultValues = gridFilter?.defaultMatcher
+    ? gridFilter.fromFilteringSpecToGridFilterModel(gridFilter.defaultMatcher)?.[field]?.values
+    : undefined;
 
-filterParams: {
-  ...extendedColDef.filterParams,
-  ...(gridFilter?.defaultSelection && { defaultSelection: gridFilter.defaultSelection }),
-},
+  columnDefsDictionary[field] = {
+    ...columnDefsDictionary[field],
+    ...extendedColDef,
+    ...(defaultValues && extendedColDef.filterParams && {
+      filterParams: {
+        ...extendedColDef.filterParams,
+        defaultSelection: defaultValues,
+      },
+    }),
+    suppressHeaderMenuButton: true,
+  };
+}
