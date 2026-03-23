45c3273653f71cb8218a016cc3cc46d58982df7a
Wojcik <dwojcik@luxoft.com>
2026-03-23 08:09:02 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,11 +1,31 @@
-export const initialFilteringSpec: FilteringSpec = Object.values(gridFilters).flatMap(
-  (config: GridFilter | undefined) =>
-    config?.defaultMatcher ? [config.defaultMatcher] : [],
-);
+export const buildFilterModelFromSpec = (
+  filteringSpec: FilteringSpec | undefined,
+  gridFilters: Partial<Record<string, GridFilter>>,
+): Record<string, any> => {
+  const filterModel: Record<string, any> = {};
 
-export const initialSortingSpec = fromFacetedSearchSorters(
-  Object.values(gridFilters).flatMap(
-    (config: GridFilter | undefined) =>
-      config?.defaultSorter ? [config.defaultSorter] : [],
-  ),
-);
+  filteringSpec?.forEach(matcher => {
+    const gridFilter = gridFilters?.[matcher.field];
+    if (!gridFilter) {
+      logger.warn(`No grid filter found for colId: ${matcher.field}`);
+      return;
+    }
+
+    const result = gridFilter.fromFilteringSpecToGridFilterModel(matcher);
+    if (!result) return;
+
+    for (const [key, val] of Object.entries(result) as [string, any][]) {
+      if (filterModel[key]) {
+        filterModel[key] = {
+          ...filterModel[key],
+          values: [...filterModel[key].values, ...val.values],
+        };
+      } else {
+        filterModel[key] = val;
+      }
+    }
+  });
+
+  return filterModel;
+};
+const newFilterModel = buildFilterModelFromSpec(filteringSpec, gridFilters);
