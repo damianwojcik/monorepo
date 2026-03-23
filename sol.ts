1dcfde3a7d7a96e35d55cb9ed1071e76950c2f4b
Wojcik <dwojcik@luxoft.com>
2026-03-23 07:51:38 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,3 +1,11 @@
-export const initialFilteringSpec: FilteringSpec = 
-  Object.values(gridFilters)
-    .flatMap(config => config?.defaultMatcher ? [config.defaultMatcher] : []);
+export const initialFilteringSpec: FilteringSpec = Object.values(gridFilters).flatMap(
+  (config: GridFilter | undefined) =>
+    config?.defaultMatcher ? [config.defaultMatcher] : [],
+);
+
+export const initialSortingSpec = fromFacetedSearchSorters(
+  Object.values(gridFilters).flatMap(
+    (config: GridFilter | undefined) =>
+      config?.defaultSorter ? [config.defaultSorter] : [],
+  ),
+);
