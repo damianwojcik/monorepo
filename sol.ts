8ab831c7e85922cdf3e8c816cd7a0cfe5b92d77e
Wojcik <dwojcik@luxoft.com>
2026-03-23 07:45:02 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,15 +1,3 @@
-type GridFilterConfig<TFilterValue = unknown> = {
-  filter: typeof ExternalFilter;
-  filterParams: ExternalFilterProps;
-  // querySpec → ag-grid filter model
-  fromFilteringSpecToGridFilterModel: (matcher: Matcher) => ColFilterModel;
-  // ag-grid filter model → querySpec
-  fromGridFilterModelToFilteringSpecs: (values: string[]) => FilteringSpec;
-  // initial querySpec matcher for this field
-  defaultMatcher: Matcher | undefined;
-};
-
 export const initialFilteringSpec: FilteringSpec = 
-  Object.entries(gridFilters)
-    .map(([_, config]) => config.defaultMatcher)
-    .filter(Boolean);
+  Object.values(gridFilters)
+    .flatMap(config => config?.defaultMatcher ? [config.defaultMatcher] : []);
