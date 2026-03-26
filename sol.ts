2dbafa8a8e99bc3be34afab5ad2faf71463f5e0e
Wojcik <dwojcik@luxoft.com>
2026-03-26 10:38:47 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,15 +1,7 @@
-useEffect(() => {
-  if (!storageLoaded.current && filteringSpec) {
-    storageLoaded.current = true;
-
-    const correctedSpec = filteringSpec.map(matcher => {
-      const gridFilter = gridFilters?.[matcher.field];
-      return gridFilter?.initialMatcher ?? matcher;
-    });
-
-    updateQuerySpec(filters.PROPELLANT_V1_FILTERING_SPEC, {
-      filteringSpec: correctedSpec,
-    });
-    return;
-  }
-}, [filteringSpec]);
+const initialSortingSpec = useMemo(
+  () =>
+    Object.values(gridFilters ?? {}).flatMap(
+      (config) => config.initialQuerySpec?.sortingSpec ?? []
+    ),
+  [gridFilters]
+);
