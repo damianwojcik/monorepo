a251e583dc8d68ce0040537573d221691ca5d50e
Wojcik <dwojcik@luxoft.com>
2026-03-26 11:45:26 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,7 +1,33 @@
 const initialSortingSpec = useMemo(
   () =>
-    Object.values(gridFilters ?? {}).flatMap(
-      (config) => config.initialQuerySpec?.sortingSpec ?? []
+    Object.values(inColumnFiltersConfig.gridFilters ?? {}).flatMap(config =>
+      'initialQuerySpec' in config ? (config?.initialQuerySpec?.sortingSpec ?? []) : []
     ),
-  [gridFilters]
+  [],
 );
+
+const {
+  items: {
+    isin: [isin],
+  },
+} = useSharedIdentifierContext<ContextItems>();
+
+useEffect(() => {
+  if (!storageLoaded.current && filteringSpec && sortingSpec) {
+    storageLoaded.current = true;
+
+    const initialFilteringSpec = filteringSpec.map(filterDef => {
+      const gridFilter = inColumnFiltersConfig.gridFilters?.[filterDef.field];
+      return gridFilter?.initialMatcher ?? filterDef;
+    });
+
+    updateQuerySpec(PROPELLANT_V1_FILTERING_SPEC, {
+      filteringSpec: initialFilteringSpec,
+    });
+    updateQuerySpec(FACETED_SEARCH_V1_FILTERING_SPEC, {
+      sortingSpec: initialSortingSpec,
+    });
+
+    return;
+  }
+}, [filteringSpec, sortingSpec]);
