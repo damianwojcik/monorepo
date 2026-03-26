0371936bcfe5d2e78cda130a7a7c27a644fd5ad5
Wojcik <dwojcik@luxoft.com>
2026-03-26 11:55:56 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,33 +1,6 @@
-const initialSortingSpec = useMemo(
-  () =>
-    Object.values(inColumnFiltersConfig.gridFilters ?? {}).flatMap(config =>
-      'initialQuerySpec' in config ? (config?.initialQuerySpec?.sortingSpec ?? []) : []
-    ),
-  [],
-);
+type GridFilterKeys = keyof typeof inColumnFiltersConfig.gridFilters;
 
-const {
-  items: {
-    isin: [isin],
-  },
-} = useSharedIdentifierContext<ContextItems>();
-
-useEffect(() => {
-  if (!storageLoaded.current && filteringSpec && sortingSpec) {
-    storageLoaded.current = true;
-
-    const initialFilteringSpec = filteringSpec.map(filterDef => {
-      const gridFilter = inColumnFiltersConfig.gridFilters?.[filterDef.field];
-      return gridFilter?.initialMatcher ?? filterDef;
-    });
-
-    updateQuerySpec(PROPELLANT_V1_FILTERING_SPEC, {
-      filteringSpec: initialFilteringSpec,
-    });
-    updateQuerySpec(FACETED_SEARCH_V1_FILTERING_SPEC, {
-      sortingSpec: initialSortingSpec,
-    });
-
-    return;
-  }
-}, [filteringSpec, sortingSpec]);
+const initialFilteringSpec = filteringSpec.map(filterDef => {
+  const gridFilter = inColumnFiltersConfig.gridFilters?.[filterDef.field as GridFilterKeys];
+  return gridFilter?.initialMatcher ?? filterDef;
+});
