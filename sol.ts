5f50d5afb95c3e73b13ebeac37ffe90da7b7e8ee
Wojcik <dwojcik@luxoft.com>
2026-03-27 07:41:41 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,4 +1,26 @@
-const initialFilteringSpec = filteringSpec.map(filterDef => {
-  const gridFilter = inColumnFiltersConfig.gridFilters?.[filterDef.field as keyof typeof inColumnFiltersConfig.gridFilters];
-  return gridFilter?.initialQuerySpec?.filteringSpec ?? filterDef;
-});
+export const convertGridFilterModelToFilteringSpec = ({
+  model,
+  gridFilters,
+  resetToInitial = false,
+}: {
+  model: FilterModel;
+  gridFilters?: GridFilters;
+  resetToInitial?: boolean;
+}): FilteringSpec => {
+  console.log('!!! convert', { model, gridFilters });
+
+  if (!gridFilters) return [];
+
+  return Object.entries(gridFilters).flatMap(([colId, gridFilter]) => {
+    if (!gridFilter) return [];
+
+    if (resetToInitial) {
+      return gridFilter.initialQuerySpec?.filteringSpec ?? [];
+    }
+
+    const values = model[colId]?.values;
+    if (!values) return [];
+
+    return gridFilter.toFilteringSpec(values) ?? [];
+  });
+};
