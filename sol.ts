8f31a6cf99ac4b48c01050fe780a76d3cc39f22d
Wojcik <dwojcik@luxoft.com>
2026-03-27 09:20:43 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -1,56 +1,18 @@
 export const convertGridFilterModelToFilteringSpec = (
-  model: FilterModel,
-  gridFilters?: GridFilters,
+  model: FilteringModelFilters,
+  gridFilters: GridFilters,
   resetToInitial = false,
-): FilteringSpec => {
-  if (!gridFilters) return [];
-
-  if (resetToInitial) {
-    return Object.values(gridFilters).flatMap(gridFilter => {
-      if (!gridFilter) return [];
-      return gridFilter.initialQuerySpec?.filteringSpec ?? [];
-    });
-  }
-
-  return Object.entries(gridFilters).flatMap(([colId, gridFilter]) => {
-    if (!gridFilter) return [];
-
+): FilteringSpec =>
+  Object.entries(gridFilters).flatMap(([colId, gridFilter]) => {
     const values = model[colId]?.values;
-    if (!values?.length) return [];
 
-    return gridFilter.toFilteringSpec(values) ?? [];
+    if (values?.length) {
+      return gridFilter.toFilteringSpec(values);
+    }
+
+    if (resetToInitial) {
+      return gridFilter.initialQuerySpec?.filteringSpec ?? [];
+    }
+
+    return [];
   });
-};
-
-export const convertFilteringSpecToGridFilterModel = (
-  filteringSpec: FilteringSpec | undefined,
-  gridFilters?: GridFilters,
-): FilterModel => {
-  if (!filteringSpec?.length) return {};
-
-  const filterModel: FilterModel = {};
-
-  for (const filterDef of filteringSpec) {
-    const gridFilter = gridFilters?.[filterDef.field];
-
-    if (!gridFilter) {
-      continue;
-    }
-
-    const result = gridFilter.toGridFilterModel(filterDef);
-
-    if (!result) {
-      continue;
-    }
-
-    for (const [key, val] of Object.entries(result) as [string, any][]) {
-      filterModel[key] = {
-        ...filterModel[key],
-        values: [...(filterModel[key]?.values ?? []), ...(val.values ?? [])],
-        refresh: true,
-      };
-    }
-  }
-
-  return filterModel;
-};
