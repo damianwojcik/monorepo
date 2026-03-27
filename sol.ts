937dc3dc74b561c25b684586a7d03f885be44e0d
Wojcik <dwojcik@luxoft.com>
2026-03-27 08:31:15 +0100

    wip

--- sol.ts
+++ sol.ts
@@ -22,35 +22,26 @@ export const convertGridFilterModelToFilteringSpec = (
   });
 };
 
-export const convertFilteringSpecToGridFilterModel = (
-  filteringSpec: FilteringSpec | undefined,
+export const convertGridFilterModelToFilteringSpec = (
+  model: FilterModel,
   gridFilters?: GridFilters,
-): FilterModel => {
-  if (!filteringSpec?.length) return {};
+  resetToInitial = false,
+): FilteringSpec => {
+  if (!gridFilters) return [];
 
-  const filterModel: FilterModel = {};
-
-  for (const filterDef of filteringSpec) {
-    const gridFilter = gridFilters?.[filterDef.field];
-
-    if (!gridFilter) {
-      continue;
-    }
-
-    const result = gridFilter.toGridFilterModel(filterDef);
-
-    if (!result) {
-      continue;
-    }
-
-    for (const [key, val] of Object.entries(result) as [string, any][]) {
-      filterModel[key] = {
-        ...filterModel[key],
-        values: [...(filterModel[key]?.values ?? []), ...(val.values ?? [])],
-        refresh: true,
-      };
-    }
+  if (resetToInitial) {
+    return Object.values(gridFilters).flatMap(gridFilter => {
+      if (!gridFilter) return [];
+      return gridFilter.initialQuerySpec?.filteringSpec ?? [];
+    });
   }
 
-  return filterModel;
+  return Object.entries(gridFilters).flatMap(([colId, gridFilter]) => {
+    if (!gridFilter) return [];
+
+    const values = model[colId]?.values;
+    if (!values?.length) return [];
+
+    return gridFilter.toFilteringSpec(values) ?? [];
+  });
 };
